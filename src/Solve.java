5626e2943a262a2fd1f4f933a88c2dc150f73710
Amarjot Parmar <amarjot-parmar@hotmail.co.nz>
2017-05-27 14:10:35 +1200

    compare method added

--- src/Solve.java
+++ src/Solve.java
@@ -16,14 +16,15 @@ public class Solve
   
   private static float budget = 0f;         //total cost allowed to be used across all stages
   private static int limit = 0;             //max number devices that can be looked up
-  private static LinkedList<Float> rs;      //reliability of device n
-  private static LinkedList<Float> cs;      //cost of device n
+  private static LinkedList<Float> rs = new LinkedList<Float>();   //reliability of device n
+  private static LinkedList<Float> cs = new LinkedList<Float>();    //cost of device n
   
   private static int[] devices = new int[ NUM_STAGES ];   //id of device used at stage n
   private static int[] num_d = new int[ NUM_STAGES ];     //num devices used at stage n
   
   public static void main( String[] args )
   {
+
     try
     {
       parseArgs( args );
@@ -33,6 +34,10 @@ public class Solve
       e.printStackTrace();
       return;
     }
+
+    //System.err.println(compare(0,1));
+
+
   }
   
   /*
@@ -55,13 +60,64 @@ public class Solve
     BufferedReader br = new BufferedReader( new FileReader( args[  0 ] ) );
     String line;
     String[] tokens;
+
     while( ( line = br.readLine() ) != null )
     {
       tokens = line.split( " " );
-      rs.add( Float.parseFloat( tokens[ 0 ] ) );
-      cs.add( Float.parseFloat( tokens[ 1 ] ) );
+      rs.add(Float.parseFloat( tokens[ 0 ] ));
+      cs.add(Float.parseFloat( tokens[ 1 ] ));
     }
     br.close();
+  }
+
+  /**
+   * Takes two indexes, works out which is better for price vs performace
+   * and returns index of the better one
+   * @param x index of circuit a
+   * @param y index of circuit b
+   */
+  private static int compare(int x, int y)
+  {
+    float temp_budget = budget;
+    float reliability = rs.get(x);
+    float cost = cs.get(x);
+    float x_total_reliability = 0;
+    //what reliability we can get using multiple of x
+    while(temp_budget >= cost)
+    {
+      x_total_reliability = x_total_reliability + (1 - reliability);
+      temp_budget = temp_budget - cost;
+    }
+
+    //System.err.println("x Total Reliability : " + x_total_reliability);
+    //System.err.println("leftover budget :" + temp_budget);
+    reliability = rs.get(y);
+     cost = cs.get(y);
+    float y_total_reliability = 0;
+
+    temp_budget = budget;
+    //what reliability we can get using multiple of y
+    while(temp_budget >= cost)
+    {
+      y_total_reliability = y_total_reliability + (1 - reliability);
+      temp_budget = temp_budget - cost;
+    }
+
+    //System.err.println("y Total Reliability : " + y_total_reliability);
+    //System.err.println("leftover budget :" + temp_budget);
+
+    //Chossing one with most reliability
+    if(y_total_reliability > x_total_reliability)
+    {
+      //System.err.println("y was picked");
+      return y;
+    }
+    else
+   {
+     //System.err.println("x was picked");
+     return x;
+   }
+
   }
   
   
